e8b69b3e73a0c847db5d704446002be2080d2d5f
student <student@gnu.ac.in>
2023-10-06 12:32:42 +0530

    date: 06/10/23

--- app/src/main/java/com/example/mad_practical_9_21012021098/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021098/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_9_21012021098
 
+import android.annotation.SuppressLint
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,22 +8,32 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     lateinit var alarmanimation: AnimationDrawable
+    lateinit var heartanimation: AnimationDrawable
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val alarmimage = findViewById<ImageView>(R.id.Alarmimage)
         alarmimage.setBackgroundResource(R.drawable.alarm_animation_list)
         alarmanimation = alarmimage.background as AnimationDrawable
+
+        val heartimage = findViewById<ImageView>(R.id.Heartimage)
+        heartimage.setBackgroundResource(R.drawable.heart_animation_list)
+        heartanimation = heartimage.background as AnimationDrawable
 
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         if (hasFocus){
-            alarmanimation.start()
+            alarmanimation.start(); heartanimation.start()
         }
         else{
-            alarmanimation.stop()
+            alarmanimation.stop(); heartanimation.stop()
         }
         super.onWindowFocusChanged(hasFocus)
     }
+
+
+
 }
