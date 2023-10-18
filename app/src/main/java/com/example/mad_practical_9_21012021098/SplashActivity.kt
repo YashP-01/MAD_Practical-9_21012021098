d030f74acbc2b1a2f0902e64b399d799d2725595
student <student@gnu.ac.in>
2023-10-18 08:55:46 +0530

    date: 18/10/23

--- app/src/main/java/com/example/mad_practical_9_21012021098/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021098/SplashActivity.kt
@@ -2,10 +2,25 @@ package com.example.mad_practical_9_21012021098
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(),AnimationListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+
+
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
     }
 }
