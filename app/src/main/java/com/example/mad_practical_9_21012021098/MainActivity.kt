aad3a3ec0a0defc57b0cb90e5bcf1d3c7e6957b7
Yash <yashpatelv21@gmail.com>
2023-11-03 00:49:21 +0530

    date: 03/11/23

--- app/src/main/java/com/example/mad_practical_9_21012021098/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021098/MainActivity.kt
@@ -7,33 +7,33 @@ import android.os.Bundle
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var alarmanimation: AnimationDrawable
-    lateinit var heartanimation: AnimationDrawable
+
+    lateinit var alarmanimation : AnimationDrawable
+    lateinit var heartanimation : AnimationDrawable
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val alarmimage = findViewById<ImageView>(R.id.Alarmimage)
-        alarmimage.setBackgroundResource(R.drawable.alarm_animation_list)
-        alarmanimation = alarmimage.background as AnimationDrawable
+        val alarm : ImageView = findViewById(R.id.alarm)
+        alarm.setBackgroundResource(R.drawable.alarm_animation_list)
+        alarmanimation = alarm.background as AnimationDrawable
 
-        val heartimage = findViewById<ImageView>(R.id.Heartimage)
-        heartimage.setBackgroundResource(R.drawable.heart_animation_list)
-        heartanimation = heartimage.background as AnimationDrawable
-
+        val heart : ImageView = findViewById(R.id.heart)
+        heart.setBackgroundResource(R.drawable.heart_animation_list)
+        heartanimation = heart.background as AnimationDrawable
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
-        if (hasFocus){
-            alarmanimation.start(); heartanimation.start()
-        }
-        else{
-            alarmanimation.stop(); heartanimation.stop()
-        }
         super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            alarmanimation.start()
+            heartanimation.start()
+        }
+        else {
+            alarmanimation.stop()
+            heartanimation.stop()
+        }
     }
-
-
-
 }
--- app/src/main/java/com/example/mad_practical_9_21012021098/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021098/SplashActivity.kt
@@ -1,26 +1,55 @@
 package com.example.mad_practical_9_21012021098
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
-import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
-class SplashActivity : AppCompatActivity(),AnimationListener {
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    lateinit var logoanimation: AnimationDrawable
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val logo: ImageView = findViewById(R.id.uvpce)
+
+        logo.setBackgroundResource(R.drawable.logoimg_animation_list)
+        logoanimation = logo.background as AnimationDrawable;
+
+        val myAnimation = AnimationUtils.loadAnimation(this, R.anim.tween_animation_list)
+        logo.startAnimation(myAnimation)
+
+        // Add an animation listener to your animation
+        myAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+                // Animation started
+            }
+
+            override fun onAnimationEnd(animation: Animation) {
+                // Animation ended, start a new activity here
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+                // Animation repeated
+            }
+        })
     }
 
-    override fun onAnimationStart(p0: Animation?) {
-
-    }
-
-    override fun onAnimationEnd(p0: Animation?) {
-
-
-    }
-
-    override fun onAnimationRepeat(p0: Animation?) {
-
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            logoanimation.start()
+        } else {
+            logoanimation.stop()
+        }
     }
 }
